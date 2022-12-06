ed8073394422590fef342d354f7051459d480a33
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-12-06 10:06:00 -0300

    adicionando validacoes gabriel

--- gabrielFuncoes.js
+++ gabrielFuncoes.js
@@ -1,28 +1,51 @@
+function validacaoPositivoInteiro(a, b) {
+    if ((b > 1) && (typeof b) == ("number") && (a > 1) && (typeof a) == ("number")) {
+        return true
+    } else {
+        return false
+    }
+}
+
 function calculoJurosSimples(C, i, t) {
-    var taxaJurosConvertida = i / 100
-    var tempoEmAnos = t * 12
-    var valorFinal = C * taxaJurosConvertida * tempoEmAnos
-    return valorFinal
+    if (validacaoPositivoInteiro(C, i)) {
+        var taxaJurosConvertida = i / 100
+        var tempoEmAnos = t * 12
+        var valorFinal = C * taxaJurosConvertida * tempoEmAnos
+        return valorFinal
+    } else {
+        throw new Error('Número deve ser positivo e inteiro');
+    }
 }
 
 function calculoJurosCompostos(M, i, t) {
-    var taxaJurosConvertida = i / 100
-    var tempoEmAnos = t * 12
-    var montante = M * (1 + taxaJurosConvertida) ** tempoEmAnos
-    return parseFloat(montante.toFixed(2))
+    if (validacaoPositivoInteiro(M, i)) {
+        var taxaJurosConvertida = i / 100
+        var tempoEmAnos = t * 12
+        var montante = M * (1 + taxaJurosConvertida) ** tempoEmAnos
+        return parseFloat(montante.toFixed(2))
+    } else {
+        throw new Error('Número deve ser positivo e inteiro');
+    }
 }
 
 function calculoRentabilidadeReal(r, i) {
-    var rentabilidadeConvertida = r / 100
-    var inflacaoConvertida = i / 100
-    var valorFinal = (1 + rentabilidadeConvertida) / (1 + inflacaoConvertida) - 1
-    var valorFinalConvertidoPorcentagem = valorFinal * 100
-    return parseFloat(valorFinalConvertidoPorcentagem.toFixed(1))
+    if (validacaoPositivoInteiro(r, i)) {
+        var rentabilidadeConvertida = r / 100
+        var inflacaoConvertida = i / 100
+        var valorFinal = (1 + rentabilidadeConvertida) / (1 + inflacaoConvertida) - 1
+        var valorFinalConvertidoPorcentagem = valorFinal * 100
+        return parseFloat(valorFinalConvertidoPorcentagem.toFixed(1))
+    } else {
+        throw new Error('Número deve ser positivo e inteiro');
+    }
 }
 
 function calculoRentabilidadePassada(pa, pp) {
-    var valorFinal = (pa / pp) * 100 - 100
-    return parseFloat(valorFinal.toFixed(2))
+    if (validacaoPositivoInteiro(pa, pp)) {
+        var valorFinal = (pa / pp) * 100 - 100
+        return parseFloat(valorFinal.toFixed(2))
+    } else {
+        throw new Error('Número deve ser positivo e inteiro');
+    }
 }
-
 module.exports = { calculoJurosSimples, calculoJurosCompostos, calculoRentabilidadeReal, calculoRentabilidadePassada }
--- marcosFuncoes.test.js
+++ marcosFuncoes.test.js
@@ -1,18 +1,42 @@
 const marcosTeste = require('./marcosFuncoes');
 
-test('Cálculo de Juros simples', ()=>{
+test('Cálculo de Juros simples', () => {
     expect(marcosTeste.CalcularJurosSimples(1000, 2, 1)).toBe(240)
 })
+test('Juros simples: erro quando n < 0 ou diferente de number', () => {
+    expect(() => marcosTeste.calculoJurosSimples("1000", 2, 1)).toThrow();
+    expect(() => marcosTeste.calculoJurosSimples(-1, 2, 1)).toThrow();
+    expect(() => marcosTeste.calculoJurosSimples(1000, "2", 1)).toThrow();
+    expect(() => marcosTeste.calculoJurosSimples(1000, -2, 1)).toThrow();
+});
 
-test('Cálculo de Juros composto', ()=>{
+test('Cálculo de Juros composto', () => {
     expect(marcosTeste.CalcularJurosCompostos(1000, 2, 1)).toBe(1268.24)
 })
+test('Juros composto: erro quando n < 0 ou diferente de number', () => {
+    expect(() => marcosTeste.CalcularJurosCompostos("1000", 2, 1)).toThrow();
+    expect(() => marcosTeste.CalcularJurosCompostos(-1, 2, 1)).toThrow();
+    expect(() => marcosTeste.CalcularJurosCompostos(1000, "2", 1)).toThrow();
+    expect(() => marcosTeste.CalcularJurosCompostos(1000, -2, 1)).toThrow();
+});
 
-test('Cálculo de Rentabilidade real', ()=>{
+test('Cálculo de Rentabilidade real', () => {
     expect(marcosTeste.CalcularRentabilidadeReal(8, 4)).toBe(3.8)
 })
+test('Rentabilidade real: erro quando n < 0 ou diferente de number', () => {
+    expect(() => marcosTeste.CalcularRentabilidadeReal("8", 4)).toThrow();
+    expect(() => marcosTeste.CalcularRentabilidadeReal(-8, 4,)).toThrow();
+    expect(() => marcosTeste.CalcularRentabilidadeReal(8, "4")).toThrow();
+    expect(() => marcosTeste.CalcularRentabilidadeReal(8, -4)).toThrow();
+});
 
-test('Cálculo de Rentabilidade passada', ()=>{
+test('Cálculo de Rentabilidade passada', () => {
     expect(marcosTeste.CalcularRentabilidadePassada(14, 9)).toBe(55.56)
 })
+test('Rentabilidade passada: erro quando n < 0 ou diferente de number', () => {
+    expect(() => marcosTeste.CalcularRentabilidadePassada("14", 9)).toThrow();
+    expect(() => marcosTeste.CalcularRentabilidadePassada(-14, 9)).toThrow();
+    expect(() => marcosTeste.CalcularRentabilidadePassada(14, "9")).toThrow();
+    expect(() => marcosTeste.CalcularRentabilidadePassada(14, -9)).toThrow();
+});
 
