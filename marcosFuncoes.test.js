d758d2a4e7c654f1dc0fe6abaa920fb0be9d848f
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-12-05 22:21:28 -0300

    correcao test

--- marcosFuncoes.test.js
+++ marcosFuncoes.test.js
@@ -1,18 +1,18 @@
 const marcosTeste = require('./marcosFuncoes');
 
 test('Cálculo de Juros simples', ()=>{
-    expect(marcosTeste.CalcularJurosSimples(1000, 2, 12)).tobe(240)
+    expect(marcosTeste.CalcularJurosSimples(1000, 2, 12)).toBe(240)
 })
 
 test('Cálculo de Juros composto', ()=>{
-    expect(marcosTeste.CalcularJurosCompostos(1000, 2, 12)).tobe(1268.24)
+    expect(marcosTeste.CalcularJurosCompostos(1000, 2, 12)).toBe(1268.24)
 })
 
 test('Cálculo de Rentabilidade real', ()=>{
-    expect(marcosTeste.CalcularRentabilidadeReal(8, 4)).tobe(3.8)
+    expect(marcosTeste.CalcularRentabilidadeReal(8, 4)).toBe(3.8)
 })
 
 test('Cálculo de Rentabilidade passada', ()=>{
-    expect(marcosTeste.CalcularRentabilidadePassada(14, 9)).tobe(55.56)
+    expect(marcosTeste.CalcularRentabilidadePassada(14, 9)).toBe(55.56)
 })
 
