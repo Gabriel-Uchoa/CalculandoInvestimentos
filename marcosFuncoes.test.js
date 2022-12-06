76f48fa61456d755e39fa5f8a36821f7ba193104
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-12-05 22:07:32 -0300

    chamada de funcoes

--- marcosFuncoes.test.js
+++ marcosFuncoes.test.js
@@ -1,18 +1,18 @@
 const marcosTeste = require('./marcosFuncoes');
 
 test('Cálculo de Juros simples', ()=>{
-    expect(marcosTeste.funcao(1000, 2, 12)).tobe(240)
+    expect(marcosTeste.CalcularJurosSimples(1000, 2, 12)).tobe(240)
 })
 
 test('Cálculo de Juros composto', ()=>{
-    expect(marcosTeste.funcao(1000, 2, 12)).tobe(1268.24)
+    expect(marcosTeste.CalcularJurosCompostos(1000, 2, 12)).tobe(1268.24)
 })
 
 test('Cálculo de Rentabilidade real', ()=>{
-    expect(marcosTeste.funcao(8, 4)).tobe(3.8)
+    expect(marcosTeste.CalcularRentabilidadeReal(8, 4)).tobe(3.8)
 })
 
 test('Cálculo de Rentabilidade passada', ()=>{
-    expect(marcosTeste.funcao(9, 14)).tobe(55.55)
+    expect(marcosTeste.CalcularRentabilidadePassada(14, 9)).tobe(55.56)
 })
 
